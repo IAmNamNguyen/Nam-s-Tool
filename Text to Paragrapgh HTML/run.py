3bb2afcd8404dba7f00bf9a55785cffd458da3f9
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2023-07-08 11:18:43 +0700

    update running now!

--- Text to Paragrapgh HTML/run.py
+++ Text to Paragrapgh HTML/run.py
@@ -1,30 +1,31 @@
 import sys
-from os.join import path
-input_file = join(sys.path[0], input.txt)
-output_file = join(sys.path[0], output.txt)
-with open(input_file,'r') as i:
+from os.path import join
+input_file = join(sys.path[0], "input.txt")
+output_file = join(sys.path[0], "output.txt")
+with open(input_file,'r',encoding="utf-8") as i:
     paras = i.readlines()
-def out(html):
-    with open(output_file,'w') as o:
-        o.write(str(html))
+print(" ")  
 print("Hãy đảm bảo rằng đã đưa dữ liệu đầu vào ở file input.txt")
-check = str(input("Đã cho dữ liệu vào file input.txt chưa? (yes/no): "))
+print(" ")
+print("********************")
+print(" ")
 checknote = str(input("Có note không? (yes/no): "))
-if checknote == "yes":
-    notes = str(input("Liệt kê các dòng cần note (ngăn cách bởi dấu phẩy, viết liền):"))
+if checknote == "yes" or checknote == "y" or checknote == "ýe":
+    notes = str(input("Liệt kê các dòng cần note (ngăn cách bởi dấu phẩy, viết liền): "))
     listnote = list(map(int, notes.split(",")))
 l = 1
-if check == "yes":
-    for t in range(len(paras)):
-        if l in listnote:
-            print(f'Đã đến dòng chứa note (dòng {t}).')
-            notenum = int(input("Ghi tên note: "))
-            notetext = str(input("Nội dung note: "))
-            html = f'<p id="{t}">{paras[t]}<span id="anchor-note{notenum}" class="tooltip note1"><img src="https://filedn.com/l6Yawa51iDjJ68pgrC3ymCB/WebData/Images/item.png" title="note1"><a href="#note1" title="Xuống dưới">.</a><span class="toolttext">{notetext}</span></span></p>\n'
-            out(html)
-        else:
-            html = f'<p id="{t}">{paras[t]}</p>\n'
-            out(html)
-        l = l + 1
+for t in range(len(paras)):
+    if l in listnote:
+        print(f'Đã đến dòng chứa note (dòng {l}).')
+        notenum = int(input("Ghi tên note: "))
+        notetext = str(input("Nội dung note: "))
+        html = f'<p id="{t}">{paras[t]}<span id="anchor-note{notenum}" class="tooltip note1"><img src="https://filedn.com/l6Yawa51iDjJ68pgrC3ymCB/WebData/Images/item.png" title="note{notenum}"><a href="#note{notenum}" title="Xuống dưới">.</a><span class="toolttext">{notetext}</span></span></p>\n'
+        with open(output_file,'a',encoding="utf-8") as o:
+            o.write(str(html))
+    else:
+        html = f'<p id="{t}">{paras[t]}</p>\n'
+        with open(output_file,'a',encoding="utf-8") as o:
+            o.write(str(html))
+    l = l + 1
 
     
