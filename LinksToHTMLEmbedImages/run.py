d5eca1bdee645ddb1c8437ce5a35bef62a08d34e
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2023-08-25 10:48:18 +0700

    Update run.py

--- LinksToHTMLEmbedImages/run.py
+++ LinksToHTMLEmbedImages/run.py
@@ -11,20 +11,22 @@ print("********************")
 print(" ")
 
 params = str(input("Các cú pháp phụ cho thẻ img (title, alt, style,..)? - Y/N: "))
-if params == Y:
+if params == "Y":
     print(f"Nếu không muốn cú pháp đó, hãy gõ \"N/A\"")
     title = str(input("title?: "))
     alt = str(input("alt?: "))
     style = str(input("style?: "))
+else:
+    for t in range(len(images)):
+        html = f'<img src={images[t]} id="{t}"/>\n'
+        with open(output_file,'a',encoding="utf-8") as o:
+            o.write(str(html))
 
 html = [f"<img src=\"{images[t]}\"",f"title=\"{title}\"",f"alt=\"{alt}\"",f"style=\"{style}\"",f"id=\"{t}\"","/>"]
 
 
 
-for t in range(len(images)):
-    html = f'<img src={images[t]} id="{t}"/>\n'
-    with open(output_file,'a',encoding="utf-8") as o:
-        o.write(str(html))
+
 
 
     
