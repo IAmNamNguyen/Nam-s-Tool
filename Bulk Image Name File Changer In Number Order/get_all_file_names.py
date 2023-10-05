032fad43177552c0e6b6c9cb04e428a45a033932
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2023-10-06 00:04:37 +0700

    Update get_all_file_names.py

--- Bulk Image Name File Changer In Number Order/get_all_file_names.py
+++ Bulk Image Name File Changer In Number Order/get_all_file_names.py
@@ -1,7 +1,8 @@
 #@title Công cụ duyệt tên file và ghi tên file ra 1 file xuất dữ liệu
 import os
 import sys
-directory = str(input("Nhập đường dẫn thư mục cần duyệt: "))
+#directory = str(input("Nhập đường dẫn thư mục cần duyệt: "))
+imagespath = os.path.join(sys.path[0],"Images")
 output_file = os.path.join(sys.path[0],"output_file_names.txt")
 def write_file_names_to_txt(directory, output_file):
     with open(output_file, 'w') as f:
@@ -9,4 +10,4 @@ def write_file_names_to_txt(directory, output_file):
             f.write(filename + '\n')
 
 
-write_file_names_to_txt(directory, output_file)
+write_file_names_to_txt(imagespath, output_file)
