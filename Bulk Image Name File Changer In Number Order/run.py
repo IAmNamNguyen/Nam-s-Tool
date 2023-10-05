a47416510d09665b446a528967a6cd2ac0668574
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2023-10-06 00:01:32 +0700

    still have bugs, maybe

--- Bulk Image Name File Changer In Number Order/run.py
+++ Bulk Image Name File Changer In Number Order/run.py
@@ -1,32 +1,37 @@
 import os
 import sys
+
 input_file = os.path.join(sys.path[0],"input_file_names.txt")
-output_file = os.path.join(sys.path[0],"output_file_names.txt")
 imagespath = os.path.join(sys.path[0],"Images")
-file_ext = str(input("File Extension? - *.:"))
-fukingdumbandroidreadnumberfilenameodering = str(input('''Support Android Ordering List Images? \n
-The Android File Manager ordered the number file name as strings, \n
+
+print('''The Android File Manager ordered the number file name as strings, \n
 so if you rename the file in normal order like this: \n
 "0.jpg, 1.jpg, 2.jpg,...,99.jpg",\n
 it will order the files in the File Explorer like this: \n
 "0.jpg, 1.jpg, 10.jpg, 11.jpg,...,2.jpg , 20.jpg, 21.jpg,..." \n
 To fix that stupid sorting, we have to add zeros (0) \n
-in front of the numbers 1 to 9
+in front of the numbers 1 to 9\n
+''')
 
-'''))
+fukingdumbandroidreadnumberfilenameodering = str(input('''Support Android Ordering List Images? \n
+Yes / No: '''))
+
 with open(input_file, 'r', encoding="utf-8") as i:
     names = i.readlines()
 
-#function to rename files
-def main(imagespath, names, output_file):
-    count = 0
-    for x in range(len(names)):
-        for file in os.listdir(imagespath):
-            #check if the file name is present in the name list
-            if file in names[count]:
-                oldName = os.path.join(imagespath, file)
-                if len(names) < 10:
-                    new = f'0 + {count} + {file_ext}'
-                if 10 < len(names) < 100:
-                newName = os.path.join(imagespath, new)
+def main(imagespath, names):
+    for count, name in enumerate(names):
+        oldName = os.path.join(imagespath, name.strip())
+        if os.path.exists(oldName):
+            new = str(count)
+            if fukingdumbandroidreadnumberfilenameodering == 'Yes':
+                if 1 <= len(names) <= 999:
+                    new_filename = new.zfill(3)
+                elif 1000 <= len(names) <= 9999:
+                    new_filename = new.zfill(4)
+            else:
+                new_filename = new
+            newName = os.path.join(imagespath, new_filename)
+            os.rename(oldName, newName)
 
+main(imagespath, names)
