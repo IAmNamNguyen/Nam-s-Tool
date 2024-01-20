f772c2b83ea649560b9402f99a209dcedda0fa07
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2024-01-20 16:48:01 +0700

    Update directlink-colab.py

--- Sites Media Downloader/colab/directlink-colab.py
+++ Sites Media Downloader/colab/directlink-colab.py
@@ -4,26 +4,20 @@ from urllib.parse import urlparse as urlp
 link = sys.argv[1]
 name = sys.argv[2]
 custom_name = sys.argv[3]
-vi_tri_luu = sys.argv[4]
-no_check_certificate = sys.argv[5]
+no_check_certificate = sys.argv[4]
 
-def directdown(link, name, vi_tri_luu, no_check_certificate):
-    link = link
-    name = name
+def directdown(link, name, no_check_certificate):
     if no_check_certificate:
-      if vi_tri_luu == "Google Drive":
-        !wget -O "$name" -P /content/drive/MyDrive "$link" --no-check-certificate
-      else:
-        !wget -O "$name" "$link" --no-check-certificate
+      !wget -O "$name" "$link" --no-check-certificate
     else:
       !wget -O "$name" "$link"
 
 if custom_name:
-    directdown(link, name, vi_tri_luu, no_check_certificate)
+    directdown(link, name, no_check_certificate)
 else:
     path = urlp(link).path.split('/')
     name = path[len(path)-1]
     name = name.replace("%20","_")
     name = name.replace("%28","(")
     name = name.replace("%29",")")
-    directdown(link, name, vi_tri_luu, no_check_certificate)
+    directdown(link, name, no_check_certificate)
