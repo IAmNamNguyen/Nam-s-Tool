c2a16e6c298223bcfb981f40a9631678d7082a11
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2024-01-30 00:44:30 +0700

    Update gdrive-colab.py

--- Sites Media Downloader/colab/gdrive-colab.py
+++ Sites Media Downloader/colab/gdrive-colab.py
@@ -16,7 +16,7 @@ b = f' \"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --sav
 c = f' -O- | sed -rn \'s/.*confirm=([0-9A-Za-z_]+).*/\1\n/p\')&id={link}\"'
 d = f' -O {FILENAME} && rm -rf /tmp/cookies.txt'
 e = ' -P /content/drive/MyDrive'
-system(f'wget{a}{b}{c}{d}')
+system(f'wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate {FILEID} -O- | sed -rn \'s/.*confirm=([0-9A-Za-z_]+).*/\1\n/p\')&id={link}" -O {FILENAME} && rm -rf /tmp/cookies.txt')
 
 #if vi_tri_luu == "Google Drive":
     #!wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "$FILEID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$link" -O "$FILENAME" && rm -rf /tmp/cookies.txt -P /content/drive/MyDrive
